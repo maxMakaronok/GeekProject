cfcf00f5eefa449716fefe4d5bae246c893430e4
Max Makaronok <13oops13@gmail.com>
2014-08-28 18:59:30 +0200

    g Signed-off-by: Max Makaronok <13oops13@gmail.com>

---
+++ Common/ServiceModels/LogFilter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceModels
+{
+    [DataContract]
+    public class LogFilter
+    {
+        [DataMember]
+        public int? EventId { get; set; }
+
+        [DataMember]
+        public int? ServiceId { get; set; }
+
+        [DataMember]
+        public int? Skip { get; set; }
+
+        [DataMember]
+        public int? Take { get; set; }
+
+    }
+}
---
+++ Common/ServiceModels/LogGetMessage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using Database;
+
+namespace ServiceModels
+{
+    [DataContract]
+    public class LogGetMessage
+    {
+        [DataMember]
+        public int ItemId { get; set; }
+
+        [DataMember]
+        public string ServiceName { get; set; }
+
+        [DataMember]
+        public string EventName { get; set; }
+        
+        [DataMember]
+        public string Message { get; set; }
+
+        [DataMember]
+        public DateTime EventDate { get; set; }
+
+        [DataMember]
+        public string UserLogin { get; set; }
+       
+    }
+}
